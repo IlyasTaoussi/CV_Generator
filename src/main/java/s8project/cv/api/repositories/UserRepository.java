e72076c61e86749808ac81b8b9d0ad6d5b8d6e1d
Birkan Yildiz <ydz.birkan@example.com>
2022-05-04 12:06:22 +0200

    feat(UserRepository.java) : Update UserRepository.java
    - add search methods

--- src/main/java/s8project/cv/api/repositories/UserRepository.java
+++ src/main/java/s8project/cv/api/repositories/UserRepository.java
@@ -8,4 +8,8 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByMailAndPassword(String mail, String password);
+
+    Optional<User> findByMail(String mail);
+
+    Optional<User> findByPassword(String password);
 }
