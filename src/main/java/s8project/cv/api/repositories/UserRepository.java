7d30a070105a16f136dec134aeb25f4a77dad9ac
Ilyas Taoussi <ilyas.taoussi2000@gmail.com>
2022-05-04 10:57:20 +0200

    fix(UserRepository.java): Change Id Class to String

--- src/main/java/s8project/cv/api/repositories/UserRepository.java
+++ src/main/java/s8project/cv/api/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package s8project.cv.api.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import s8project.cv.api.documents.User;
 
-public interface UserRepository extends MongoRepository<User, Integer> {
+public interface UserRepository extends MongoRepository<User, String> {
 }
