3acd104cab494a215a46ff10bb9c6b91095ec1ee
Ilyas Taoussi <ilyas.taoussi2000@gmail.com>
2022-05-05 11:52:30 +0200

    fix(/): Git Stash Pop Miss

--- src/main/java/s8project/cv/api/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package s8project.cv.api.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import s8project.cv.api.documents.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
