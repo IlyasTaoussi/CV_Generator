fff913a21d5c82e1e424abae72331988ad6ae6f8
Ilyas Taoussi <ilyas.taoussi2000@gmail.com>
2022-05-06 11:14:38 +0200

    feat(UserRepositoryCustom): Add Insert/Update Methods for documents - Methods are used for REST PUT Methods

--- src/main/java/s8project/cv/api/repositories/UserRepositoryCustom.java
+++ src/main/java/s8project/cv/api/repositories/UserRepositoryCustom.java
@@ -1,7 +1,17 @@
 package s8project.cv.api.repositories;
 
+import s8project.cv.api.documents.*;
+
+
 public interface UserRepositoryCustom {
 
+    User findByUserId(int userId);
     int getMaxUserId();
 
+    Certification insertCert(int userId, Certification certification);
+    void insertContact(int userId, Contact contact);
+    Education insertEducation(int userId, Education education);
+    Language insertLang(int userId, Language lang);
+    ProfessionalExperience insertProfExp(int userId, ProfessionalExperience profExp);
+    Skill insertSkill(int userId, Skill skill);
 }
--- src/main/java/s8project/cv/api/repositories/UserRepositoryCustomImpl.java
+++ src/main/java/s8project/cv/api/repositories/UserRepositoryCustomImpl.java
@@ -1,17 +1,31 @@
 package s8project.cv.api.repositories;
 
+import com.mongodb.client.result.UpdateResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
-import s8project.cv.api.documents.User;
+import s8project.cv.api.documents.*;
+import s8project.cv.api.utilities.Utilities;
+
+
+import java.util.Comparator;
+import java.util.List;
 
 @Repository
 public class UserRepositoryCustomImpl implements UserRepositoryCustom{
 
     @Autowired
     MongoTemplate mongoTemplate;
+
+    @Override
+    public User findByUserId(int userId){
+        Query query = new Query(Criteria.where("userId").is(userId));
+        return mongoTemplate.findOne(query, User.class);
+    }
 
     @Override
     public int getMaxUserId() {
@@ -23,6 +37,155 @@ public class UserRepositoryCustomImpl implements UserRepositoryCustom{
             return 0;
         }
         return user.getUserId();
+    }
+
+    @Override
+    public Certification insertCert(int userId, Certification certification){
+        int newId = 0;
+
+        Query query = new Query(Criteria.where("userId").is(userId));
+        User user = mongoTemplate.findOne(query, User.class);
+        CV cv = user.getCV();
+
+        if(cv == null) {
+            cv = new CV();
+            newId = 1;
+        }
+        else{
+            List<Certification> certs = (List<Certification>) Utilities.copy(cv.getCertification());
+            certs.sort(Comparator.comparing(Certification::getId));
+            newId = certs.get(certs.size() - 1).getId() + 1;
+        }
+
+        certification.setId(newId);
+        cv.getCertification().add(certification);
+
+        Update update = new Update();
+        update.set("cv", cv);
+        mongoTemplate.updateFirst(query, update, User.class);
+        return certification;
+    }
+
+    @Override
+    public void insertContact(int userId, Contact contact){
+        Query query = new Query(Criteria.where("userId").is(userId));
+        User user = mongoTemplate.findOne(query, User.class);
+        CV cv = user.getCV();
+
+        if(cv == null) cv = new CV();
+        cv.setContact(contact);
+
+        Update update = new Update();
+        update.set("cv", cv);
+        mongoTemplate.updateFirst(query, update, User.class);
+    }
+
+    @Override
+    public Education insertEducation(int userId, Education education){
+        int newId = 0;
+
+        Query query = new Query(Criteria.where("userId").is(userId));
+        User user = mongoTemplate.findOne(query, User.class);
+        CV cv = user.getCV();
+
+        if(cv == null) {
+            cv = new CV();
+            newId = 1;
+        }
+        else{
+            List<Education> educations = (List<Education>) Utilities.copy(cv.getCertification());
+            educations.sort(Comparator.comparing(Education::getId));
+            newId = educations.get(educations.size() - 1).getId() + 1;
+        }
+
+        education.setId(newId);
+        cv.getEducation().add(education);
+
+        Update update = new Update();
+        update.set("cv", cv);
+        mongoTemplate.updateFirst(query, update, User.class);
+        return education;
+    }
+
+    @Override
+    public Language insertLang(int userId, Language lang){
+        int newId = 0;
+
+        Query query = new Query(Criteria.where("userId").is(userId));
+        User user = mongoTemplate.findOne(query, User.class);
+        CV cv = user.getCV();
+
+        if(cv == null) {
+            cv = new CV();
+            newId = 1;
+        }
+        else{
+            List<Language> langs = (List<Language>) Utilities.copy(cv.getCertification());
+            langs.sort(Comparator.comparing(Language::getId));
+            newId = langs.get(langs.size() - 1).getId() + 1;
+        }
+
+        lang.setId(newId);
+        cv.getLanguage().add(lang);
+
+        Update update = new Update();
+        update.set("cv", cv);
+        mongoTemplate.updateFirst(query, update, User.class);
+        return lang;
+    }
+
+    @Override
+    public Skill insertSkill(int userId, Skill skill){
+        int newId = 0;
+
+        Query query = new Query(Criteria.where("userId").is(userId));
+        User user = mongoTemplate.findOne(query, User.class);
+        CV cv = user.getCV();
+
+        if(cv == null) {
+            cv = new CV();
+            newId = 1;
+        }
+        else{
+            List<Skill> skills = (List<Skill>) Utilities.copy(cv.getCertification());
+            skills.sort(Comparator.comparing(Skill::getId));
+            newId = skills.get(skills.size() - 1).getId() + 1;
+        }
+
+        skill.setId(newId);
+        cv.getSkill().add(skill);
+
+        Update update = new Update();
+        update.set("cv", cv);
+        mongoTemplate.updateFirst(query, update, User.class);
+        return skill;
+    }
+
+    @Override
+    public ProfessionalExperience insertProfExp(int userId, ProfessionalExperience profExp){
+        int newId = 0;
+
+        Query query = new Query(Criteria.where("userId").is(userId));
+        User user = mongoTemplate.findOne(query, User.class);
+        CV cv = user.getCV();
+
+        if(cv == null) {
+            cv = new CV();
+            newId = 1;
+        }
+        else{
+            List<Skill> skills = (List<Skill>) Utilities.copy(cv.getCertification());
+            skills.sort(Comparator.comparing(Skill::getId));
+            newId = skills.get(skills.size() - 1).getId() + 1;
+        }
+
+        profExp.setId(newId);
+        cv.getProfessionalExperience().add(profExp);
+
+        Update update = new Update();
+        update.set("cv", cv);
+        mongoTemplate.updateFirst(query, update, User.class);
+        return profExp;
     }
 
 }
