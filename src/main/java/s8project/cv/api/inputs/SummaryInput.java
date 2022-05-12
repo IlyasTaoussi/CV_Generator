92857b06f2b44b2ece18f2f18dfec3fd5e7cd1f6
Ilyas Taoussi <ilyas.taoussi2000@gmail.com>
2022-05-12 13:41:53 +0200

    feat(SummerInput): JSON Recognition with SummaryInput

---
+++ src/main/java/s8project/cv/api/inputs/SummaryInput.java
@@ -0,0 +1,18 @@
+package s8project.cv.api.inputs;
+
+public class SummaryInput {
+
+    private String summary;
+
+    public SummaryInput(String summary) {
+        this.summary = summary;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+}
